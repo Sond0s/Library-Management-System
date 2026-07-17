d75250c234480a74af078c54fa01394575b47938
Sond0s <sondoskhalid79@gmail.com>
2026-07-17 21:16:40 +0300

    Move DAL and BLL inside the repo

--- obj/Debug/net8.0-windows/LibraryManagmentSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/LibraryManagmentSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagmentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82ad2a1d62658d475aedafac790919617a3b1c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07892468461d994280219b758581ce14ace6416f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagmentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagmentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
